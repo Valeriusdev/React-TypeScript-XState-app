ae1705596a94e80821b76f661eb942288819460f
Val Lysenko <kakao757@gmail.com>
2025-12-18 22:53:13 -0800

    update styling

--- src/pages/ToggleMachine.tsx
+++ src/pages/ToggleMachine.tsx
@@ -28,7 +28,7 @@ function ToggleMachine() {
           ← Back to Home
         </Link>
 
-        <h1 className="text-xl font-bold mb-8">XState Toggle Machine</h1>
+        <h1 className="text-xl font-bold mb-10">XState Toggle Machine</h1>
 
         <div className="mb-6">
           <p className="text-xl mb-2">Current State:</p>
